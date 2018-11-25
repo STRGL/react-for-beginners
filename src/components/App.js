79091c0c9ea263746691f250cd5b4608dd38881c
STRGL <daryllclarke@hotmail.co.uk>
2018-11-25 13:16:34 +0000

    Initial Commit. Finished lesson 13

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+
+class App extends React.Component {
+
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = (fish) => {
+        //1. Take a cope of the existing state - you don't want to modify it directly
+        const fishes = { ...this.state.fishes };
+        //2.add new fish to fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        //3.set new fishes object to state
+        this.setState({
+            //can remove one fishes since they both have the same name, but I've left it in for clarity
+            fishes: fishes
+        });
+
+    }
+
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline='Fresh Seafood Market' />
+                </div>
+                <Order />
+                <Inventory addFish={this.addFish} />
+            </div>
+
+        )
+    }
+}
+
+export default App;
